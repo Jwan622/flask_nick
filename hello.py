577677af047dd755938f5e4fa7f75d6bc31d4006
Jeffrey <jwan622@gmail.com>
2019-12-15 20:36:29 -0500

    flask beginning

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from nicks_code import ComplicatedThing
+from flask import json
+app = Flask(__name__)
+
+@app.route("/jeff")
+def hello():
+	data = {'mydata': ComplicatedThing().perform_other_calc()}
+
+	response = app.response_class(
+		response=json.dumps(data),
+		status=200,
+		mimetype='application/json'
+	)
+
+	return response
+
+@app.route("/nick")
+def nick():
+	return ComplicatedThing().perform_other_calc()
+
+if __name__ == "__main__":
+    app.run()
---
+++ nicks_code.py
@@ -0,0 +1,6 @@
+class ComplicatedThing:
+	def perform_calc(self):
+		return "something ccompli9cated"
+
+	def perform_other_calc(self):
+		return "something else ocmplicated"
